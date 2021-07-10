b15bcc1345d0bac6ab715bed23a16106439cdf6b
beavercoffee <beavercoffee@vidsoe.com>
2021-07-09 23:04:31 -0600

    BC

--- classes/class-bc-cf7-edit-user.php
+++ classes/class-bc-cf7-edit-user.php
@@ -52,45 +52,6 @@ if(!class_exists('BC_CF7_Edit_User')){
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    	private function copy($source = '', $destination = '', $overwrite = false, $mode = false){
-            global $wp_filesystem;
-            $fs = $this->filesystem();
-            if(is_wp_error($fs)){
-                return $fs;
-            }
-            if(!$wp_filesystem->copy($source, $destination, $overwrite)){
-                return new WP_Error('files_not_writable', sprintf(__('The uploaded file could not be moved to %s.'), $destination));
-            }
-            return $destination;
-        }
-
-    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    	private function filesystem(){
-            global $wp_filesystem;
-            if($wp_filesystem instanceof WP_Filesystem_Direct){
-                return true;
-            }
-            if(!function_exists('get_filesystem_method')){
-                require_once(ABSPATH . 'wp-admin/includes/file.php');
-            }
-            if('direct' !== get_filesystem_method()){
-                return new WP_Error('fs_unavailable', __('Could not access filesystem.'));
-            }
-            if(!WP_Filesystem()){
-                return new WP_Error('fs_error', __('Filesystem error.'));
-            }
-            return true;
-        }
-
-    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    	private function first_p($text = '', $dot = true){
-            return $this->one_p($text, $dot, 'first');
-        }
-
-    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
         private function get_user_id($contact_form = null, $submission = null){
             if(null === $contact_form){
                 return new WP_Error('bc_error', __('The requested contact form was not found.', 'contact-form-7'));
@@ -121,7 +82,7 @@ if(!class_exists('BC_CF7_Edit_User')){
             }
             if(null !== $nonce and !wp_verify_nonce($nonce, 'bc-edit-user_' . $user_id)){
                 $message = __('The link you followed has expired.');
-                $message .=  ' ' . $this->last_p(__('An error has occurred. Please reload the page and try again.'));
+                $message .=  ' ' . bc_last_p(__('An error has occurred. Please reload the page and try again.'));
                 return new WP_Error('bc_error', $message);
             }
             $user_id = $this->sanitize_user_id($user_id);
@@ -150,40 +111,6 @@ if(!class_exists('BC_CF7_Edit_User')){
                 return '';
             }
             return $type;
-        }
-
-    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    	private function last_p($text = '', $dot = true){
-            return $this->one_p($text, $dot, 'last');
-        }
-
-    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    	private function one_p($text = '', $dot = true, $p = 'first'){
-            if(false === strpos($text, '.')){
-                if($dot){
-                    $text .= '.';
-                }
-                return $text;
-            } else {
-                $text = explode('.', $text);
-				$text = array_filter($text);
-                switch($p){
-                    case 'first':
-                        $text = array_shift($text);
-                        break;
-                    case 'last':
-                        $text = array_pop($text);
-                        break;
-                    default:
-                        $text = __('Error');
-                }
-                if($dot){
-                    $text .= '.';
-                }
-                return $text;
-            }
         }
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -218,32 +145,11 @@ if(!class_exists('BC_CF7_Edit_User')){
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     	private function upload_file($tmp_name = '', $post_id = 0){
-            global $wp_filesystem;
-            $upload_dir = wp_upload_dir();
-            $original_filename = wp_basename($tmp_name);
-            $filename = wp_unique_filename($upload_dir['path'], $original_filename);
-            $file = trailingslashit($upload_dir['path']) . $filename;
-            $result = $this->copy($tmp_name, $file);
-            if(is_wp_error($result)){
-                return $result;
+            $file = bc_move_uploaded_file($tmp_name);
+            if(is_wp_error($file)){
+                return $file;
             }
-            $filetype_and_ext = wp_check_filetype_and_ext($file, $filename);
-            if(!$filetype_and_ext['type']){
-                return new WP_Error('invalid_filetype', __('Sorry, this file type is not permitted for security reasons.'));
-            }
-            $attachment_id = wp_insert_attachment([
-                'guid' => str_replace($upload_dir['basedir'], $upload_dir['baseurl'], $file),
-                'post_mime_type' => $filetype_and_ext['type'],
-                'post_status' => 'inherit',
-                'post_title' => preg_replace('/\.[^.]+$/', '', $original_filename),
-            ], $file, $post_id, true);
-            if(is_wp_error($attachment_id)){
-                return $attachment_id;
-            }
-            $attachment = get_post($attachment_id);
-            wp_raise_memory_limit('image');
-            wp_maybe_generate_attachment_metadata($attachment);
-            return $attachment_id;
+            return bc_upload($file, $post_id);
         }
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -272,6 +178,9 @@ if(!class_exists('BC_CF7_Edit_User')){
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         public function plugins_loaded(){
+            if(!defined('BC_FUNCTIONS')){
+        		return;
+        	}
             if(!defined('WPCF7_VERSION')){
         		return;
         	}
@@ -283,6 +192,7 @@ if(!class_exists('BC_CF7_Edit_User')){
             if(!has_filter('wpcf7_verify_nonce', 'is_user_logged_in')){
                 add_filter('wpcf7_verify_nonce', 'is_user_logged_in');
             }
+            bc_build_update_checker('https://github.com/beavercoffee/bc-cf7-edit-user', $this->file, 'bc-cf7-edit-user');
         }
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -340,7 +250,7 @@ if(!class_exists('BC_CF7_Edit_User')){
             do_action('bc_cf7_edit_user', $user_id, $contact_form, $error);
             if($error->has_errors()){
                 $message = $error->get_error_message();
-                $message .=  ' ' . $this->last_p(__('Application passwords are not available for your account. Please contact the site administrator for assistance.'));
+                $message .=  ' ' . bc_last_p(__('Application passwords are not available for your account. Please contact the site administrator for assistance.'));
                 $submission->set_response($message);
                 $submission->set_status('aborted');
                 return;
